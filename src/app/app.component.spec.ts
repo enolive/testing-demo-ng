42b1cb3ffc05ad2e1a16ff41d1c4d9184b61f284
Christoph Welcz <christoph@welcz.de>
2022-01-09 07:18:48 +0100

    🎉 complete code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,70 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component'
+import {fireEvent, render, waitFor} from '@testing-library/angular'
+import {HttpClient} from '@angular/common/http'
+import {createMock, Mock} from '@testing-library/angular/jest-utils'
+import {EMPTY, of, throwError} from 'rxjs'
+import {JokeResponse} from './types'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+  let httpMock: Mock<HttpClient>
+
+  const renderComponent = async () =>
+    await render(AppComponent, {
+      providers: [
+        {provide: HttpClient, useValue: httpMock},
       ],
-    }).compileComponents();
-  });
+    })
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  beforeEach(() => {
+    httpMock = createMock(HttpClient)
+    httpMock.get.mockReturnValue(EMPTY)
+  })
 
-  it(`should have as title 'testing-demo-ng'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('testing-demo-ng');
-  });
+  it('renders correctly', async () => {
+    const {getByRole} = await renderComponent()
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testing-demo-ng app is running!');
-  });
-});
+    expect(getByRole('heading')).toHaveTextContent('Welcome to my app')
+    expect(getByRole('img')).toMatchSnapshot()
+    expect(getByRole('button')).toHaveTextContent('Refresh')
+    expect(getByRole('status')).toHaveTextContent('Loading...')
+  })
+
+  it('fetches random joke on load', async () => {
+    const response: JokeResponse = {
+      value: '👊 BAM 👊',
+    }
+    httpMock.get.mockReturnValue(of(response))
+
+    const {getByRole} = await renderComponent()
+
+    await waitFor(() =>
+      expect(getByRole('status')).toHaveTextContent('👊 BAM 👊'),
+    )
+    expect(httpMock.get).toHaveBeenCalledWith('https://api.chucknorris.io/jokes/random')
+  })
+
+  it('fetches random joke on button click', async () => {
+    const response1: JokeResponse = {value: '👊 BAM 👊'}
+    const response2: JokeResponse = {value: '👊 BOOM 👊'}
+    httpMock.get
+      .mockReturnValueOnce(of(response1))
+      .mockReturnValueOnce(of(response2))
+    const {getByRole} = await renderComponent()
+    fireEvent.click(getByRole('button'))
+
+    await waitFor(() =>
+      expect(getByRole('status')).toHaveTextContent('👊 BOOM 👊'),
+    )
+  })
+
+  it('displays errors', async () => {
+    const response = throwError(() => new Error('NOOOO!'))
+    httpMock.get.mockReturnValue(response)
+
+    const {getByRole} = await renderComponent()
+
+    await waitFor(() =>
+      expect(getByRole('status')).toHaveTextContent('NOOOO!'),
+    )
+  })
+})
